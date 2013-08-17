4bbe60ce627cf18684b90babcf5f5c813228113b
tmaeno <tmaeno@d4488797-78bc-619e-cc48-e4164af92aee>
2013-08-17 06:19:44 +0000

    typo

--- current/pandaserver/dataservice/AdderPluginBase.py
+++
@@ -1,8 +0,0 @@
-from AdderResult import AdderResult
-
-class AdderPluginBase(object):
-    def __init__(self,job,params):
-        self.job = job
-        self.result = AdderResult()
-        for tmpKey,tmpVal in params.iteritems():
-            setattr(self,tmpKey,tmpVal)
